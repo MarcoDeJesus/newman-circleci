42211f4a9e07a481003932fc756ae117929b1eb9
Marco De Jesus <marco.dejesus@offerup.com>
2021-10-06 22:07:44 -0500

    Adding double appersand to run sequentially and not in parallel.

--- test-suites/test-suites.sh
+++ test-suites/test-suites.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-npm run test-random-jokes -- ${npm_config_environment:-'-e' 'environments/staging.postman_environment.json'}  & 
-npm run test-random-jokes-per-category -- ${npm_config_environment:-'-e' 'environments/staging.postman_environment.json'} & 
-npm run test-random-together-jokes -- ${npm_config_environment:-'-e' 'environments/staging.postman_environment.json'} & 
+npm run test-random-jokes -- ${npm_config_environment:-'-e' 'environments/staging.postman_environment.json'}  && 
+npm run test-random-jokes-per-category -- ${npm_config_environment:-'-e' 'environments/staging.postman_environment.json'} &&
+npm run test-random-together-jokes -- ${npm_config_environment:-'-e' 'environments/staging.postman_environment.json'} && 
 npm run test-random-together-jokes-per-category -- ${npm_config_environment:-'-e' 'environments/staging.postman_environment.json'}
